30180fd08f8ea9333fb51ebe3224be4f89f5aa3e
alemendonc <160601834+alemendonc@users.noreply.github.com>
2024-02-27 08:33:26 -0300

    oiyh

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ function mudarCorTexto(){
 
     alert("hello")
     console.log(document.querySelector("#p1"))
-    document.querySelector("#p1").style.color="green"
+    document.querySelector("#p1").style.color="white"
 }
