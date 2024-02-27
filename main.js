8ee7ca932b11084230ad1867d83d1bd57330f926
alemendonc <160601834+alemendonc@users.noreply.github.com>
2024-02-27 08:38:16 -0300

    jkb

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ function mudarCorTexto(){
     console.log(document.querySelector("#p1"))
     document.querySelector("#p1").style.color="white"
 }
+
+function mudarCorFundo(){
+    document.querySelector("#p1").style.background="white"
+
+}
