bc86bf013661087b9db756e7ceb13d0d8709f852
alemendonc <160601834+alemendonc@users.noreply.github.com>
2024-02-27 16:00:54 -0300

    dth

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function mudarCorTexto(){
 
     alert("hello")
     console.log(document.querySelector("#p1"))
-    document.querySelector("#p1").style.color="black"
+    document.querySelector("#p1").style.color="green"
 }
 
 function mudarCorFundo(){
